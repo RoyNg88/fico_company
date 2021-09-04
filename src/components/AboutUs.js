391b94927d195f77b697f016409b48684306054a
andrei0202 <56860673+andrei0202@users.noreply.github.com>
2021-09-04 23:56:53 +0700

    firstCommit
    firstCommit

---
+++ src/components/AboutUs.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faMobileAlt, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+
+class AboutUs extends Component{
+        render(){
+            return(
+                <div id = "aboutUs">
+                    <div className="content_aboutus">
+                    <h1>About Us</h1>
+                    <div className="contentAbout">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </p>
+                    </div>
+                    <FontAwesomeIcon icon={faMapMarkerAlt} size="4x" className="icon_css" href="#"/>
+                    <FontAwesomeIcon icon={faMobileAlt} size="4x" className="icon_css" href="#" />
+                    <FontAwesomeIcon icon={faEnvelope} size="4x"  className="icon_css" href="#" />
+
+
+                    
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    
+                    
+                    </div>
+
+                    <h2>Meet The Team</h2>
+                    <div className="container_team"></div>
+                    <div className="profile_team">
+                        <div className="profile_member">
+                        <img className ="profile-img" src="https://cq.ru/storage/uploads/players/1140368/1.jpg" ></img>
+                        <h3 className ="user-name">Huy Bui</h3>
+                        <h5>Front End Developer</h5>
+                        </div>
+                        <div className="profile_member">
+                        <img className ="profile-img" src="https://cq.ru/storage/uploads/players/1140368/1.jpg" ></img>
+                        <h3 className ="user-name">Dang Quang</h3>
+                        <h5>Front End Developer</h5>
+                        </div>
+                        <div className="profile_member">
+                        <img className ="profile-img" src="https://cq.ru/storage/uploads/players/1140368/1.jpg" ></img>
+                        <h3 className ="user-name">Phong Nguyen</h3>
+                        <h5>Front End Developer</h5>
+                        </div>
+                        <div className="profile_member">
+                        <img className ="profile-img" src="https://cq.ru/storage/uploads/players/1140368/1.jpg" ></img>
+                        <h3 className ="user-name">Gia Minh</h3>
+                        <h5>Front End Developer</h5>
+                        </div>
+                        <div className="profile_member">
+                        <img className ="profile-img" src="https://cq.ru/storage/uploads/players/1140368/1.jpg" ></img>
+                        <h3 className ="user-name">Cat Duy</h3>
+                        <h5>Front End Developer</h5>
+                        </div>
+                        <div className="profile_member">
+                        <img className ="profile-img" src="https://cq.ru/storage/uploads/players/1140368/1.jpg" ></img>
+                        <h3 className ="user-name">Huy Bui</h3>
+                        <h5>Front End Developer</h5>
+                        </div>
+                    
+
+                    </div>
+                    
+
+
+                    
+
+
+                </div>
+            );
+
+        }
+
+}
+
+export default AboutUs;
